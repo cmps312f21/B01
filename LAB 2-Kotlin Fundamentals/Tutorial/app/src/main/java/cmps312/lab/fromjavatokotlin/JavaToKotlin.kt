f8d25fba52f77aaaac525a99658385195609e095
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-08-26 16:52:43 +0300

    first part of the lab

---
+++ LAB 2-Kotlin Fundamentals/Tutorial/app/src/main/java/cmps312/lab/fromjavatokotlin/JavaToKotlin.kt
@@ -0,0 +1,90 @@
+package cmps312.lab.fromjavatokotlin
+
+/*  Print and variables
+    println("Hello")
+    print("Hello")
+
+    //Variable declarations
+    //final String var1 = "Something"
+    //type inference
+    val var1 = "Something"
+
+    println(var1)
+
+    var x = 10
+    x += 20
+
+    println(x)
+
+    //always declare your variables as val
+ */
+
+/*
+    val name = "Mohanad"
+    var age = 10
+    var gender = "Male"
+
+    //String interpolation
+
+    println("Name = $name Age = $age Gender = $gender")
+    println("Name = $name Age = $age Gender = $gender Status = ${if (age > 18) "adult" else "Child"}")
+
+//    System.out.printf("Name = %s Age = %d Gender = %s" , name , age , gender)
+//    System.out.printf("Name = " + name + " Age = " + age)
+
+//    Null
+
+    val lastname: String? = null
+
+    if (lastname != null) {
+        println("The length of the name is ${lastname.length}")
+    }
+
+    println("The length of the name is ${lastname?.length}")
+
+    println(
+        """
+        Name : $name
+        Age : $age
+        Gender : $gender
+    """.trimIndent()
+    )
+
+    //instanceof
+//    if(name instanceof String){
+//
+//    }
+
+    //is a relation
+    if (name is String)
+        println("Yes the name is a string")
+
+    // && and , || or
+
+//    var allowed = age > 18 ? "yes" : "no"
+
+    var allowed = if (age > 18) "yes" else "no"
+
+    age = 10
+
+//    if (age < 13)
+//        println("child")
+//    else if ((age < 21) and (age > 13))
+//        println("Teenager")
+//    else
+//        println("You are old")
+
+
+    //when replacement for the switch case
+    //score 6..10 excelelnt
+    // 6 good
+    // bad
+
+    age = 18
+    when(age){
+        10,11,44,5,6,6,7 -> println("you are either 10 or 11")
+        in 14..21-> println("teenager")
+        18-> println("you are 18")
+        else -> println("old")
+    }
+ */
---
+++ LAB 2-Kotlin Fundamentals/Tutorial/app/src/main/java/cmps312/lab/fromjavatokotlin/main.kt
@@ -0,0 +1,16 @@
+package cmps312.lab.fromjavatokotlin
+
+//if java was created in 2021
+
+fun main() {
+
+
+    //loops
+
+    //functions
+
+    //collections + lamdas
+
+
+
+}
