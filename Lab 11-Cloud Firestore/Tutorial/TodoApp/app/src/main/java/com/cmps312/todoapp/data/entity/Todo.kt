00343346cd6a20a5ab9cd175eeec8140407f2c78
Abdulahi Hassen <abdala.baaji@gmail.com>
2021-11-04 16:38:06 +0300

    update 4:38

---
+++ Lab 11-Cloud Firestore/Tutorial/TodoApp/app/src/main/java/com/cmps312/todoapp/data/entity/Todo.kt
@@ -0,0 +1,14 @@
+package com.cmps312.todoapp.data.entity
+
+import com.google.firebase.firestore.DocumentId
+
+data class Todo(
+    var title: String? = "",
+    var priority: String? = "",
+    var date: String? = "",
+    var projectId: String? = "",
+
+    //todo make it an document id
+    @DocumentId
+    var id: String = "",
+)
